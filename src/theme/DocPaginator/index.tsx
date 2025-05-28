b2c0ab116e79522d023f69eafc2ceb095a91cf5a
godlikeswan <godlikeswan@gmail.com>
2025-05-28 21:40:38 +0000

    feat: deleted pagination links

---
+++ src/theme/DocPaginator/index.tsx
@@ -0,0 +1,44 @@
+import { type ReactNode } from "react";
+import React from "react";
+// import clsx from "clsx";
+// import Translate, { translate } from "@docusaurus/Translate";
+// import PaginatorNavLink from "@theme/PaginatorNavLink";
+import type { Props } from "@theme/DocPaginator";
+
+export default function DocPaginator(_props: Props): ReactNode {
+  // const { className, previous, next } = props;
+  return null;
+  // <nav
+  //   className={clsx(className, 'pagination-nav')}
+  //   aria-label={translate({
+  //     id: 'theme.docs.paginator.navAriaLabel',
+  //     message: 'Docs pages',
+  //     description: 'The ARIA label for the docs pagination',
+  //   })}>
+  //   {previous && (
+  //     <PaginatorNavLink
+  //       {...previous}
+  //       subLabel={
+  //         <Translate
+  //           id="theme.docs.paginator.previous"
+  //           description="The label used to navigate to the previous doc">
+  //           Previous
+  //         </Translate>
+  //       }
+  //     />
+  //   )}
+  //   {next && (
+  //     <PaginatorNavLink
+  //       {...next}
+  //       subLabel={
+  //         <Translate
+  //           id="theme.docs.paginator.next"
+  //           description="The label used to navigate to the next doc">
+  //           Next
+  //         </Translate>
+  //       }
+  //       isNext
+  //     />
+  //   )}
+  // </nav>
+}
